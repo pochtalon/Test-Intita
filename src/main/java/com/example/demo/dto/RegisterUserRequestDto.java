cdee30eec1f8a0f815e8b7acbcb25b45a9fbde48
pochtalon <pochtalon1010@gmail.com>
2023-12-22 23:08:00 +0200

    first commit

---
+++ src/main/java/com/example/demo/dto/RegisterUserRequestDto.java
@@ -0,0 +1,28 @@
+package com.example.demo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class UserRequestDto {
+    @NotBlank
+    @Size(min = 8, max = 50)
+    private String login;
+    @NotBlank
+    @Size(min = 8, max = 100)
+    private String password;
+    @NotBlank
+    @Size(min = 8, max = 100)
+    private String passwordRepeat;
+    @NotBlank
+    @Size(max = 50)
+    private String firstName;
+    @NotBlank
+    @Size(max = 50)
+    private String lastName;
+    private String about;
+    private String address;
+}
